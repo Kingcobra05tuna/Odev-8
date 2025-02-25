7839801cf1cf91dae458c8dfbb5a97015741d667
Kingcobra05tuna <tunagulagac@gmail.com>
2025-02-25 18:09:36 +0300

    Odev 8 yeni

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,18 +1,25 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import edu.wpi.first.wpilibj.PS5Controller;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Swerve;
 
 public class RobotContainer {
-  PS5Controller kumanda = new PS5Controller(0);
+  public static Swerve swerve = new Swerve();
 
   public RobotContainer() {
+    // Configure the trigger bindings
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`,
+  }
 
-    public Command getAutonomousCommand() {
+  public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     return null;
   }
